3ced8254798d7de66e24a555b35f77f4bc3a69c7
cheshta-kabra <64300181+cheshta-kabra@users.noreply.github.com>
2022-09-05 18:14:50 +0530

    Add files via upload

--- project.py
+++ project.py
@@ -1,88 +1,88 @@
 from flask import Flask,jsonify,request
 import csv 
-all_books=[]
+all_articles=[]
 with open('articles.csv',encoding="utf8") as f:
     reader=csv.reader(f)
     data=list(reader)
-    all_books=data[1:]
+    all_articles=data[1:]
 liked=[]
 disliked=[]
 did_not_read=[]
 app=Flask(__name__)
 
-@app.route('/get-book')
-def get_book():
+@app.route('/get-article')
+def get_article():
     return jsonify({
-        'data':all_books[0],
+        'data':all_articles[0],
         'status':'success'
     })
 
-@app.route('/liked-books',methods=['POST'])
-def liked_book():
-    book=all_books[0]
-    all_books=all_books[1:]
-    liked.append(book)
+@app.route('/liked-articles',methods=['POST'])
+def liked_article():
+    article=all_articles[0]
+    all_articles=all_articles[1:]
+    liked.append(article)
     return jsonify({
         'status':'success'
     })
-@app.route('/disliked-books',methods=['POST'])
-def disliked_book():
-    book=all_books[0]
-    all_books=all_books[1:]
-    disliked.append(book)
+@app.route('/disliked-articles',methods=['POST'])
+def disliked_article():
+    article=all_articles[0]
+    all_articles=all_articles[1:]
+    disliked.append(article)
     return jsonify({
         'status':'success'
     })
-@app.route('/did-not-read-books',methods=['POST'])
-def did_not_read_book():
-    book=all_books[0]
-    all_books=all_books[1:]
-    did_not_read.append(book)
+@app.route('/did-not-read-articles',methods=['POST'])
+def did_not_read_article():
+    article=all_articles[0]
+    all_articles=all_articles[1:]
+    did_not_read.append(article)
     return jsonify({
         'status':'success'
     })
 
-@app.route("/popular-books")
-def popular_books():
-    book_data = []
-    for book in output:
+@app.route("/popular-articles")
+def popular_articles():
+    article_data = []
+    for article in output:
         _d = {
-            "id": book[0],
-            "authorRegion": book[9],
-            "authorCountry": book[10] or "N/A",
-            "contentType": book[11],
-            "url": book[12],
-            "title": book[13]
+            "id": article[0],
+            "authorRegion": article[9],
+            "authorCountry": article[10] or "N/A",
+            "contentType": article[11],
+            "url": article[12],
+            "title": article[13]
         }
-        book_data.append(_d)
+        article_data.append(_d)
     return jsonify({
-        "data": book_data,
+        "data": article_data,
         "status": "success"
     }), 200
 
-@app.route("/recommended-books")
-def recommended_books():
+@app.route("/recommended-articles")
+def recommended_articles():
     all_recommended = []
-    for liked_book in liked_books:
-        output = get_recommendations(liked_book[19])
+    for liked_article in liked_articles:
+        output = get_recommendations(liked_article[19])
         for data in output:
             all_recommended.append(data)
     import itertools
     all_recommended.sort()
     all_recommended = list(all_recommended for all_recommended,_ in itertools.groupby(all_recommended))
-    book_data = []
+    article_data = []
     for recommended in all_recommended:
         _d = {
-             "id": book[0],
-            "authorRegion": book[9],
-            "authorCountry": book[10] or "N/A",
-            "contentType": book[11],
-            "url": book[12],
-            "title": book[13]
+             "id": article[0],
+            "authorRegion": article[9],
+            "authorCountry": article[10] or "N/A",
+            "contentType": article[11],
+            "url": article[12],
+            "title": article[13]
         }
-        book_data.append(_d)
+        article_data.append(_d)
     return jsonify({
-        "data": book_data,
+        "data": article_data,
         "status": "success"
     }), 200
 
